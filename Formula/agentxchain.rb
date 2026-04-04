e0b5a2eb9b53f01f0dbc6d39bf63481dca772314
Shivam Tiwari <shivamtiwari1011@gmail.com>
2026-04-04 00:26:34 -0400

    agentxchain 2.9.0

--- Formula/agentxchain.rb
+++ Formula/agentxchain.rb
@@ -1,8 +1,8 @@
 class Agentxchain < Formula
   desc "CLI for AgentXchain governed multi-agent software delivery"
   homepage "https://agentxchain.dev"
-  url "https://registry.npmjs.org/agentxchain/-/agentxchain-2.8.0.tgz"
-  sha256 "9f4ae4fd704198f406bc0f0a353e75df1a3ffd6a12576430614a8c3b9439212f"
+  url "https://registry.npmjs.org/agentxchain/-/agentxchain-2.9.0.tgz"
+  sha256 "712421c5cf8bf20f66ff71930db0351d2a3ee29588b70663347aad448b92b703"
   license "MIT"
 
   depends_on "node"
